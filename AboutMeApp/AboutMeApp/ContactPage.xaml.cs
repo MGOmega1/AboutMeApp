ed62c0061a98ec96254d5140a57298ed1c38e1f9
0735767 <mgriffin@gus.pittstate.edu>
2018-10-24 18:12:04 -0500

    modified contact page to include message submission with validation for name, email, and message. Also modified skills page to incorporate a skills listview and changed corne radius for a frame in bio page.

--- AboutMeApp/AboutMeApp/ContactPage.xaml.cs
+++ AboutMeApp/AboutMeApp/ContactPage.xaml.cs
@@ -31,5 +31,26 @@ namespace AboutMeApp
         {
             Device.OpenUri(new Uri("https://www.linkedin.com/in/michael-griffin-b65063b6/"));
         }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(nameEntry.Text))
+            {
+                DisplayAlert("Error", "Please Enter a Name", "Ok");
+                return;
+            }
+            if (string.IsNullOrEmpty(emailEntry.Text))
+            {
+                DisplayAlert("Error", "Please Enter your Email", "Ok");
+                return;
+            }
+            if (string.IsNullOrEmpty(messageEditor.Text))
+            {
+                DisplayAlert("Error", "Please Enter a Message", "Ok");
+                return;
+            }
+
+            DisplayAlert("Alert", "Message has been Submitted", "Ok");
+        }
     }
 }
--- AboutMeApp/AboutMeApp/SkillsPage.xaml.cs
+++ AboutMeApp/AboutMeApp/SkillsPage.xaml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.ObjectModel;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -12,9 +13,18 @@ namespace AboutMeApp
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class SkillsPage : ContentPage
 	{
+        public ObservableCollection<string> Skills { get; set; }
 		public SkillsPage ()
 		{
 			InitializeComponent ();
+            this.Skills = new ObservableCollection<string>();
 		}
-	}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            this.Skills.Add(skillAdd.Text);
+            skillAdd.Text = "";
+            skillsListView.ItemsSource = this.Skills;
+        }
+    }
 }
